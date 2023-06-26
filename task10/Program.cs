46de17e6b2502d58b75865d74cd05581fc55638e
Eduard_Yuldashev <4akaaaa@gmail.com>
2023-06-26 14:15:13 +0300

    start

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,13 @@
-﻿Console.Clear();
-int 
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа*/
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число");
+string userInput1 = Console.ReadLine() ?? "";
+int num = int.Parse(userInput1);
+int a1 = num / 10;
+int a2 = num % 10;
+int res = a2;
+Console.WriteLine("{num} -> {res}");
+
+
